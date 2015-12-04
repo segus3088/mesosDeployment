0cdb6af0eeb875b32659918b4dbf3066639f0b26
SEBASTIAN SALAZAR <segus3088@gmail.com>
2015-12-04 11:20:47 -0500

    scrip to build MESOS

---
+++ bildinMESOS.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Se busca para compilar MESOS
+
+echo "iniciando Descarga"
+wget http://www.apache.org/dist/mesos/0.21.0/mesos-0.21.0.tar.gz
+
+echo "extrac pakage"
+tar –xzf mesos-*.tar.gz
+rm mesos-*.tar.gz ; mv mesos-* mesos
+cd mesos
+
+echo "Buildin Dir Buil"
+mkdir build
+cd build
+sh ../configure
