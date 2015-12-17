d1a5947e7d4ec760cb8ccb99c6a0385f787e75dc
SEBASTIAN SALAZAR <segus3088@gmail.com>
2015-12-17 17:15:58 -0500

    iniciado conection to slaves to install

--- installSlaves.sh
+++ installSlaves.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 #Para compiar e instalar en los slaves
 #Se Compia la carpeta compilada de mesos
+
+echo "host Al que se va a instalar"
+read hostSlaveI
+#nombre del host al que voy a instalar
+
+echo "User to acces host ${hostSlaveI}"
+read userToHost
+
+scp *.sh $userToHost@$hostSlaveI:~
+ssh $userToHost@$hostSlaveI
+sh installRequirement.sh
